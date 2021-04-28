f317fa07607bcd290908031d8926df86472836e9
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2021-04-28 12:52:32 +0530

    Fixed Routes And Controller Namespaces

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-     protected $namespace = 'App\\Http\\Controllers';
+     protected $namespace = 'App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -38,9 +38,9 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('api/v1')
                 ->middleware('api')
-                ->namespace($this->namespace.'\\API\\V1')
+                ->namespace($this->namespace.'\API\V1')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
